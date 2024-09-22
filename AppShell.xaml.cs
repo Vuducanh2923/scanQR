ad266779233c9ff71f7db55f7594498e1f26df5e
Vuducanh2923 <Vuducanh2923@gmail.com>
2024-09-22 18:52:58 +0700

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui_IOT1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ScanPage),typeof(ScanPage));
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Maui_IOT1
+{
+    [QueryProperty(nameof(QrCodeResult), "Thiết bị phát hiện ")]
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public string QrCodeResult
+        {
+            set
+            {
+                CounterBtn.Text=$"Scanner result :{value}";
+            }
+        }
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private async void ScanButtonClick(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(ScanPage));
+        }
+    }
+
+}
